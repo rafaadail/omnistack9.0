47642c3138c3b57fb3f68838241402db094221dc
rafaadail <rafaadailph@gmail.com>
2019-10-03 18:05:20 -0300

    Criando a base da aplicação.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-cmhn9.mongodb.net/semanda09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 //GET, POST, PUT, DELETE
 
