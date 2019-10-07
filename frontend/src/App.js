8af1e6f19f5ada5ce71bc30726d04e68e5b03867
rafaadail <rafaadailph@gmail.com>
2019-10-07 18:35:58 -0300

    Ajuste Layout Login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos para sua empresa.</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form>
+          <label htmlFor="email">E_MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail" />
+            <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
+    
+    
   );
 }
 
