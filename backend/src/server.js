7bd938efffcaa83efb47c59b855a044a9d034418
rafaadail <rafaadailph@gmail.com>
2019-11-14 17:23:11 -0300

    Instalação expo.

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-cmhn9.mongodb.net/s
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
